69e3a91f593a3ca8a6723314eac3ef6bf0c76c66
Nancy Reyes <n01188031@humber.ca>
2023-08-16 19:46:52 -0400

    CRUD for Types

---
+++ resources/views/types/list.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-quiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dashboard</title>
+
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="/app.css">
+
+        <script src="/app.js"></script>
+    </head>  
+    <body>
+        <header class="w3-padding">
+            <h1 class="w3-text-red">Project Dashboard</h1>
+
+            <?php if(Auth::check()): ?>
+                You are logged in as <?= auth()->user()->first ?> <?= auth()->user()->last ?> | 
+                <a href="/console/logout">Log Out</a> | 
+                <a href="/console/dashboard">Dashboard</a> | 
+                <a href="/">Website Home Page</a>
+            <?php else: ?>
+                <a href="/">Return to My Portfolio</a>
+            <?php endif; ?>
+
+        <header>
+
+        <hr>
+
+        <?php if(session()->has('message')): ?>
+
+            <div class="w3-padding w3-margin-top w3-margin-bottom">
+                <div class="w3-red w3-center w3-padding">
+                    <?= session()->get('message') ?>
+                </div>
+            </div>
+        
+        <?php endif; ?>
+
+        <section class="w3-padding">
+            <h2>Manage Types</h2>
+
+            <table class="w3-table w3-stripped w3-bordered w3-margin-bottom">
+                <tr class="w3-red">
+                    <th></th>
+                    <th>Title</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <?php foreach($types as $key =>$value): ?>
+                    <tr>
+                        <td><?= $value->id ?></td>
+                        <td><?= $value->title ?></td>
+                        <td><a href="/console/types/edit/<?= $value->id ?>">Edit</a></td>
+                        <td><a href="/console/types/delete/<?= $value->id ?>">Delete</a></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+
+            <a href="/console/types/add" class="w3-button w3-green">Add</a>
+
+        </section>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ConsoleController;
+use App\Http\Controllers\TypesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,10 @@ Route::get('/console/dashboard', [ConsoleController::class, 'dashboard'])->middl
 Route::get('/console/login', [ConsoleController::class, 'loginForm'])->middleware('guest')->name('login'); /* when the page loads - displays form */
 Route::post('/console/login', [ConsoleController::class, 'login'])->middleware('guest'); /* when form is submitted / process form - check email and pass to see if account is valid*/
 Route::get('/console/logout', [ConsoleController::class, 'logout'])->middleware('auth'); /* logout process*/
+
+Route::get('/console/types/list', [TypesController::class, 'list'])->middleware('auth');
+Route::get('/console/types/delete/{type:id}', [TypesController::class, 'delete'])->where('type', '[0-9]+')->middleware('auth');
+Route::get('/console/types/add', [TypesController::class, 'addForm'])->middleware('auth'); /* displays form */
+Route::post('/console/types/add', [TypesController::class, 'add'])->middleware('auth'); /*  process the form*/
+Route::get('/console/types/edit/{type:id}', [TypesController::class, 'editForm'])->where('type', '[0-9]+')->middleware('auth');
+Route::post('/console/types/edit/{type:id}', [TypesController::class, 'edit'])->where('type', '[0-9]+')->middleware('auth');
